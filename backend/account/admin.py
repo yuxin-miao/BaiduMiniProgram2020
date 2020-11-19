88d67a686ba898fa938e389d4d8fe614fdfc3cf0
nichujie <ncj19991213@126.com>
2020-11-19 16:02:57 +0800

    backend: fix user admin

--- backend/account/admin.py
+++ backend/account/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
 from account.models import User
 
-admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
