05523d4c9ecc694397852e9ecdce5d3d6d7c8e72
nichujie <ncj19991213@126.com>
2020-11-19 16:30:23 +0800

    backend: fix restful

--- backend/account/serializers.py
+++ backend/account/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from django.contrib.auth.models import User
+from account.models import User
 
 
 class LoginSerializer(serializers.Serializer):
@@ -15,4 +15,4 @@ class UserDetailSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = User
-        fields = ('id', 'username',)
+        fields = ('id', 'username', 'gender', 'avatar', 'openid', 'session_id')
