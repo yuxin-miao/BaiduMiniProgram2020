e31e83a374cbd9bf5c96739466d4c7ceaed1c27e
nichujie <ncj19991213@126.com>
2020-11-19 14:05:00 +0800

    backend: user migration

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'account',
     'rest_framework',
 ]
 
@@ -86,6 +87,8 @@ DATABASES = {
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
+
+AUTH_USER_MODEL = 'account.User'
 
 AUTH_PASSWORD_VALIDATORS = [
     {
--- backend/command.sh
+++ backend/command.sh
@@ -2,4 +2,5 @@
 
 python manage.py collectstatic
 cp -r ./static/. ./frontend/build/static
+python manage.py migrate
 python manage.py runserver 0.0.0.0:8000
