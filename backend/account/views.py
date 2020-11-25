e90393cab8d4a8cdf3919fa8cef5ebff729e3396
nichujie <ncj19991213@126.com>
2020-11-25 16:42:15 +0800

    try

--- backend/account/views.py
+++ backend/account/views.py
@@ -64,7 +64,7 @@ class AccountViewSet(viewsets.ViewSet):
                     openid=openid,
                     session_key=session_key
                 )
-                user.set_password(openid)
+                # user.set_password(openid)
             except Exception:
                 print('百度用户数据错误')
                 return Response({'detail': '百度用户数据错误'}, status=status.HTTP_400_BAD_REQUEST)
